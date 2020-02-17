05cede56195a1f91240ba417ec80c3bc86f78b5d
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-02-17 23:09:49 +0100

    feat: initial commit

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Sidebar from "components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+};
+
+export default App;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Sidebar = () => {
+  return (
+    <nav className={styles.sidebar}>
+      <ul>
+        <li>
+          <a href='#'>
+            f
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+};
+
+export default Sidebar;
